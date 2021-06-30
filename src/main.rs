764f1217a20e71aa0e1e08d48cca8facbc380acb
John Trammell <John.Trammell@target.com>
2021-06-30 15:49:26 -0500

    tweak main() to work with stable 0.4 rocket version

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 
 mod records;
 mod utils;
 
-#[rocket::main]
-async fn main(){
-    let _ = rocket::build()
-        .mount("/",routes![
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![
             records::get_records,
             records::get_records_by_id,
             records::create_record,
             records::update_record,
-            records::delete_record
-        ])
-        .register("/", catchers![utils::not_found])
-        .launch()
-        .await;
+            records::delete_record])
+        .register(catchers![utils::not_found])
+        .launch();
 }
