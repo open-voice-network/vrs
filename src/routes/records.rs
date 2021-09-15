f9baeecd4b7b6d9745e4861f9ab1ea6fd447ef0e
maridob <maria.dobrowski@yahoo.com>
2021-09-15 14:43:24 -0500

    added the get all call from db

--- src/routes/records.rs
+++ src/routes/records.rs
@@ -3,6 +3,7 @@ use rocket::*;
 use rocket_contrib::json::Json;
 use rocket_contrib::json;
 use rocket::response::status;
+use rocket_contrib::uuid::Uuid;
 
 use rocket_contrib::json::JsonValue;
 
@@ -13,31 +14,44 @@ use bson::{bson, doc, Bson};
 use mongodb::db::ThreadedDatabase;
 use mongodb::coll::options::{ReturnDocument, FindOneAndUpdateOptions};
 
-// use crate::data::db::{Record, InsertableRecord, ResponseRecord, UserPassword};
 use crate::data::db::{Record, InsertableRecord, ResponseRecord};
 use crate::data::mongo_connection::Conn;
 use crate::routes::responses::ApiResponse;
+use crate::data::security::JwtGuard;
 
 const COLLECTION: &str = "records";
 
 
-
 #[get("/records")]
-pub fn get_records() -> JsonValue {
-    
-    json!([{"id": 1, "name":"Jane Smith"}, {"id": 2, "name":"Jose Brinas"}])
+pub fn get_records(connection: Conn, _guard : JwtGuard) -> ApiResponse {
+    let record_coll = &connection.collection(COLLECTION);
+    match  record_coll.count(None, None) {
+        Ok(res) => ApiResponse::ok(json!([res])),
+        Err(_) => ApiResponse::internal_err(),
+    }   
 }
 
 
 #[get("/records/<id>")]
-pub fn get_records_by_id(id: i32) -> JsonValue {
-    json!({"id": id, "name":"Jane Smith"})
+pub fn get_records_by_id(connection: Conn, id: Uuid, _guard : JwtGuard) -> ApiResponse {
+    let record_coll = &connection.collection(COLLECTION);
+    let id =  id.to_string();
+    match record_coll.find_one(Some(doc! { "_id": id.clone() }), None) {
+        Ok(find_one) => {
+            match find_one {
+                Some(found_record) => {
+                    let found_record_doc: Result<Record, _> = bson::from_bson(Bson::Document(found_record));
+                    match found_record_doc {
+                        Ok(found_record) => ApiResponse::ok(json!(ResponseRecord::from_record(&found_record))),
+                        Err(_) => ApiResponse::internal_err(),
+                    }
+                }
+                None => ApiResponse::err(json!(format!("id {} not found",  id)))
+            }
+        },
+        Err(_) => ApiResponse::internal_err(),
+    }
 }
-
-// #[post("/records", format = "json")]
-// pub fn create_record() -> JsonValue {
-//     json!({"id": 1, "name":"Jane Smith"})
-// }
 
 #[post("/records", format = "json", data = "<record>")]
 pub fn create_record(connection: Conn, record: Json<InsertableRecord>) -> ApiResponse {
